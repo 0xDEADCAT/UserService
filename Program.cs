cf67927a5e50d28b85c18d957202ed3adc866c22
Dawid Osuchowski <dwdosuchowski@gmail.com>
2022-05-12 22:49:26 +0200

    Fix CORS from any origin

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ builder.Services.AddCors(options =>
     options.AddDefaultPolicy(
         builder =>
         {
-            builder.AllowAnyOrigin()
+            builder.SetIsOriginAllowed(_ => true)
                 .AllowAnyHeader()
                 .AllowAnyMethod()
                 .AllowCredentials();
